0cd1e36577add6d7ed8777528d700d937fd350c1
vgneushev <vgneushev@it-one.ru>
2024-08-22 18:38:52 +0300

    [S9] Add Sign-in: Authentication and Authorization

---
+++ src/main/java/com/devdemo/app/ws/mobiledemoappws/security/AuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.devdemo.app.ws.mobiledemoappws.security;
+
+import com.devdemo.app.ws.mobiledemoappws.SpringApplicationContext;
+import com.devdemo.app.ws.mobiledemoappws.service.UserService;
+import com.devdemo.app.ws.mobiledemoappws.shared.dto.UserDto;
+import com.devdemo.app.ws.mobiledemoappws.ui.model.request.UserLoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        try {
+
+            UserLoginRequestModel creds = new ObjectMapper().readValue(request.getInputStream(), UserLoginRequestModel.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        byte[] secretKeyBytes = Base64.getEncoder().encode(SecurityConstants.getTokenSecret().getBytes());
+        SecretKey secretKey = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS512.getJcaName());
+        Instant now = Instant.now();
+
+        String userName = ((User) auth.getPrincipal()).getUsername();
+        String token = Jwts.builder()
+                .setSubject(userName)
+                .setExpiration(
+                        Date.from(now.plusMillis(SecurityConstants.EXPIRATION_TIME)))
+                .setIssuedAt(Date.from(now)).signWith(secretKey, SignatureAlgorithm.HS512).compact();
+
+        UserService userService = (UserService) SpringApplicationContext.getBean("userServiceImpl");
+        final UserDto userDto = userService.getUser(userName);
+        res.addHeader(SecurityConstants.AUTH_HEADER, SecurityConstants.TOKEN_PREFIX + token);
+        res.addHeader(SecurityConstants.USER_ID_HEADER, userDto.getUserId());
+    }
+}
--- src/main/java/com/devdemo/app/ws/mobiledemoappws/security/WebSecurity.java
+++ src/main/java/com/devdemo/app/ws/mobiledemoappws/security/WebSecurity.java
@@ -4,10 +4,12 @@ import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,11 +32,20 @@ public class WebSecurity {
         AuthenticationManagerBuilder authenticationManagerBuilder
                 = http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        AuthenticationFilter filter = new AuthenticationFilter(authenticationManager);
+        filter.setFilterProcessesUrl("/users/login");
 
         http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
-                (authz) -> authz.requestMatchers(HttpMethod.POST, "/users")
-                        .permitAll()
-                        .anyRequest().authenticated());
+                        (authz) -> authz.requestMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+                                .permitAll()
+                                .anyRequest().authenticated())
+                .authenticationManager(authenticationManager)
+                .addFilter(filter)
+                .addFilter(new AuthorizationFilter(authenticationManager))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
         return http.build();
     }
 }
