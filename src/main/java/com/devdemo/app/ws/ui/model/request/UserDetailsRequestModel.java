3e4419b6600961e967d603562437166526459e49
vgneushev <vgneushev@it-one.ru>
2024-08-27 16:19:20 +0300

    [S19] add addresses models

--- src/main/java/com/devdemo/app/ws/ui/model/request/UserDetailsRequestModel.java
+++ src/main/java/com/devdemo/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -2,10 +2,13 @@ package com.devdemo.app.ws.ui.model.request;
 
 import lombok.Data;
 
+import java.util.Collection;
+
 @Data
 public class UserDetailsRequestModel {
         private String firstName;
         private String lastName;
         private String email;
         private String password;
+        private Collection<AddressRequestModel> address;
 }
